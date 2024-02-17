e8971169f3f56a4e097a311095eeea2309d9e5a4
ross-barone <rrb6815@rit.edu>
2024-02-17 14:38:37 -0500

    Created Pour Scene

---
+++ Tend the Tavern/Assets/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    protected Vector2 mousePos= Vector2.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
